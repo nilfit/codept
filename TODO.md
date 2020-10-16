99e0b58ce301a016f5b0c94206d74ae8a5facc80
nilfit <nils.fitinghoff@gmail.com>
2020-10-17 00:53:14 +0200

    it works
    but it is not very neatly installable

---
+++ TODO.md
@@ -0,0 +1,1 @@
+TODO package, fix the paths.
---
+++ prep-db.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#
+# invoke like `./prep-db.py </usr/share/unicode/extracted/DerivedName.txt >unicode.txt`
+#
+# TODO might want to use fileinput https://docs.python.org/3/library/fileinput.html
+
+import sys
+
+# Example input
+# 0039          ; DIGIT NINE
+# 003A          ; COLON
+
+# Desired output
+# 9 DIGIT NINE
+# : COLON
+
+for line in sys.stdin:
+    parts = [s.strip() for s in line.split(';')]
+    # Most non-codepoint lines will get dropped here
+    if len(parts) == 1: continue
+    try:
+        # Hex to unicode char
+        parts[0] = chr(int(parts[0], base=16))
+        print(parts[0], parts[1])
+    except ValueError:
+        # truncate out the trailing newline
+        print("WARNING skipping line: ", line[:-1], file=sys.stderr)
